4a99e7686cee32fbbe7229abdf835db2aed5f4b6
emingarip <emingarip@users.noreply.github.com>
2026-03-20 11:34:57 +0000

    deploy: 0829cf9a42f0af486c263c74703c4b77ca80fd85

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "217973623" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3068183915" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
